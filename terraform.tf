a17bdad284febc244b704fa188cf01fe0987b9ad
dddaaammmooo <dddaaammmooo@users.noreply.github.com>
2021-03-31 12:59:00 +1100

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~> 3.1.0"
+      version = "~> 3.34.0"
     }
     local = {
       source = "hashicorp/local"
